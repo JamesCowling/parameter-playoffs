e8f930c15a4b8d4d2d057208a36ea6e2457f00d7
James Cowling <james@convex.dev>
2023-10-04 18:44:23 -0700

    fix env var

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $CONTEXT = "deploy-preview"  ] ; then 
-	export NEXT_PUBLIC_CONVEX_URL=$(npx convex preview $HEAD --run 'init:reset') && npm run build
+	export VITE_CONVEX_URL=$(npx convex preview $HEAD --run 'init:reset') && npm run build
 elif [ $CONTEXT = "production" ]; then  
    npm run build && npx convex deploy
 else
